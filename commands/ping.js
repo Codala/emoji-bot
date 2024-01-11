aba5fb52c42f9107b5599d7fea505e97a9754abf
DevVali <90271108+DevVali@users.noreply.github.com>
2024-01-11 20:48:16 +0100

    Initial commit

---
+++ commands/ping.js
@@ -0,0 +1,8 @@
+module.exports = {
+    code: `Pong! I responded in $executionTimems`,
+    data: {
+        name: "ping",
+        description: "A simple ping command.",
+        type: 1,
+    },
+};
---
+++ index.js
@@ -0,0 +1,11 @@
+const { ForgeClient } = require("forgescript");
+const { token } = require("./config.json");
+
+const client = new ForgeClient({
+    intents: ["GuildMessages", "Guilds"],
+    events: ["messageCreate", "ready"],
+});
+
+client.login(token);
+
+client.applicationCommands.load("./commands");
