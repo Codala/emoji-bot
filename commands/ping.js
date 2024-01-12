623b930f37059ebeb1b7a1fed1d14d2da1356fd2
DevVali <90271108+DevVali@users.noreply.github.com>
2024-01-12 23:59:12 +0100

    feat: add steal emoji command

--- commands/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	code: `Pong! I responded in $executionTimems`,
-	data: {
-		name: "ping",
-		description: "A simple ping command.",
-		type: 1,
-	},
-};
---
+++ commands/steal.js
@@ -0,0 +1,42 @@
+module.exports = {
+	code: `
+	$let[emoji;https://cdn.discordapp.com/emojis/$option[emoji]]
+
+	$onlyIf[$hasPerms[$guildID;$authorID;ManageGuildExpressions];$ephemeral You are missing the \`ManageGuildExpressions\` permissions.]
+	$onlyIf[$httpRequest[$get[emoji];get]==200;$ephemeral The request didn't succeed. Make sure you provided a valid emoji ID.]
+
+	$if[$addEmoji[$guildID;$option[name];$get[emoji];true]!=;
+	$title[New emoji]
+	$thumbnail[$get[emoji]]
+	$description[The emoji was added to \`$guildName[$guildID]\` successfully.]
+	$footer[Added by $username]
+	$timestamp
+	$color[#91c587]
+	;
+	$ephemeral An unexpected error occurred.
+	]
+	`,
+	data: {
+		name: "steal",
+		description: "Steal an emoji from another server.",
+		type: 1,
+		options: [
+			{
+				type: 3,
+				name: "emoji",
+				description: "Emoji ID",
+				required: true,
+				min_length: 17,
+				max_length: 19,
+			},
+			{
+				type: 3,
+				name: "name",
+				description: "Name of the new emoji",
+				required: true,
+				min_length: 2,
+				max_length: 32,
+			},
+		],
+	},
+};
